567d1ff9c5762c04fd27fa14983d58c1e5886a25
brkantyldz <mberkant.yldz@outlook.com>
2024-08-06 01:54:30 +0300

    Add dataset and script files

--- deneme_streamlit_son.py
+++ deneme_streamlit_son.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:225c6f02d92ac1c30792205b60a2368c38f62aaa2d41a045675f8bb3849cb960
-size 6662
+oid sha256:6bd7ac81f8444e604bd4842968ef8a3bd7d7dbca20443181698b626659266d5a
+size 7086
